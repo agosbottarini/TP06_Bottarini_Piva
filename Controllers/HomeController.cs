22e41980a5a7385731a89d70e0fc5ce02a6095f4
AGOSTINA BOTTARINI <47866440@ort>
2023-06-13 11:26:36 -0300

    creando proyecto

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TP6. Elecciones 2023.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
